2efe72e479614d5e4a0b29de4916615f21231be3
Ugo25 <hugoacosta7911@gmail.com>
2025-11-02 23:09:13 -0700

    cambios en idiomas

--- src/i18n.js
+++ src/i18n.js
@@ -3,34 +3,33 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Importa tus archivos de traducción
 import translationEN from './locales/en/translation.json';
 import translationES from './locales/es/translation.json';
-import translationFR from './locales/fr/translation.json'; // <-- Ya está importado
+import translationFR from './locales/fr/translation.json'; 
 
 // Los recursos (traducciones)
 const resources = {
+    es: {
+    translation: translationES
+  },
   en: {
     translation: translationEN
   },
-  es: {
-    translation: translationES
-  },
-  fr: { // <-- YA ESTÁ DESCOMENTADO
+  fr: { 
     translation: translationFR
   }
 };
 
 i18n
-  .use(LanguageDetector) // Detecta el idioma del usuario
-  .use(initReactI18next) // Pasa i18n a react-i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'es', // Idioma por defecto
-    debug: true, // Desactívalo en producción
+    fallbackLng: 'es',
+    debug: true,
 
     interpolation: {
-      escapeValue: false // React ya se encarga de escapar valores (XSS)
+      escapeValue: false
     },
     react: {
       useSuspense: true,
